d607b0ade5cf5efc0c042ba959cecf92a3332e25
Neal Piche <phirestalker@gmail.com>
2025-08-31 05:50:32 -0700

    try different signing

--- Formula/pam-ssh-agent.rb
+++ Formula/pam-ssh-agent.rb
@@ -23,7 +23,7 @@ class PamSshAgent < Formula
         depends_on "libssh" => :build
         depends_on "openssl@3" => :build
         # For codesigning
-        # depends_on :xcode => :build
+        depends_on :xcode => :build
     end
 
     def install
@@ -43,13 +43,16 @@ class PamSshAgent < Formula
         # The library is built as `libpam_ssh_agent.so` or `libpam_ssh_agent.dylib`.
         # We need to install it as `pam_ssh_agent.so` in the standard PAM location
         # within the Homebrew prefix.
-        lib_name = shared_library("libpam_ssh_agent")
+        lib_name = shared_library("pam_ssh_agent")
         (lib/"security").install "target/release/#{lib_name}" => "pam_ssh_agent.so"
 
         if OS.mac?
-            # On macOS, the compiled library must be signed to be loaded by system processes
-            # that are protected by the Hardened Runtime.
-            system "/usr/bin/codesign", "--force", "--sign", "-", "--options=runtime", lib/"security/pam_ssh_agent.so"
+            # On modern macOS, an ad-hoc signature is insufficient for PAM modules.
+            # You must sign it with a local code signing certificate.
+            # See the caveats for instructions on how to create one.
+            # Replace "pam-signer" with the name of the certificate you created if different.
+            signing_identity = "pam-signer"
+            system "/usr/bin/codesign", "--force", "--sign", signing_identity, "--options=runtime", lib/"security/pam_ssh_agent.so"
         end
     end
 
@@ -59,48 +62,51 @@ class PamSshAgent < Formula
         The module was installed to:
         #{lib}/security/pam_ssh_agent.so
 
-        **macOS Note:** This formula has built a statically-linked, self-contained
-        module and signed it to comply with system security policies.
+        ------------------------------------------------------------------
+        **macOS Security Configuration (IMPORTANT):**
 
-        **macOS Instructions:**
+        This formula has built a statically-linked module. However, due to macOS's
+        Hardened Runtime, you MUST sign the module with a locally created code
+        signing certificate for it to be loaded by system processes like `sudo`.
 
-        You do NOT need to create a symlink. Edit the PAM configuration for
-        the service you want (e.g., `/etc/pam.d/sudo`) and add the following
-        line at the top, using the full path:
+        **Instructions to Create and Trust a Signing Certificate:**
+
+        1. Open the "Keychain Access" application.
+        2. In the menu bar, go to: Keychain Access > Certificate Assistant > Create a Certificate...
+        3. Set the following options:
+        - Name: pam-signer  (The formula uses this name by default)
+        - Identity Type: Self-Signed Root
+        - Certificate Type: Code Signing
+        4. Click "Create", and then "Done". Accept any default prompts.
+        5. In Keychain Access, find the new "pam-signer" certificate (usually in the "login" keychain).
+        6. Double-click the certificate, expand the "Trust" section, and set
+        "When using this certificate:" to "Always Trust". Close the window and
+        enter your password when prompted.
+
+        **After creating the certificate, reinstall this formula to apply the signature:**
+        brew reinstall ./pam-ssh-agent.rb
+        ------------------------------------------------------------------
+
+        **macOS PAM Configuration:**
+
+        Edit the PAM configuration for the service you want (e.g., `/etc/pam.d/sudo`)
+        and add the following line at the top, using the full path:
 
         auth       sufficient     #{lib}/security/pam_ssh_agent.so
-
-        You will need root privileges to edit this file, for example:
-        sudo nano /etc/pam.d/sudo
-
-        **Linux Instructions:**
-
-        1. First, create a symlink from the installed module to your system's
-        PAM directory (e.g., /lib/x86_64-linux-gnu/security/):
-
-        sudo ln -sf "#{lib}/security/pam_ssh_agent.so" /lib/x86_64-linux-gnu/security/
-
-        2. Next, edit the PAM configuration file (e.g., `/etc/pam.d/sudo`)
-        and add this line at the top:
-
-        auth       sufficient     pam_ssh_agent.so
-
-        **General Configuration Options:**
-
-        To use a specific set of authorized keys, you can add the `file` parameter:
-        auth       sufficient     ...so file=~/.ssh/authorized_keys
         EOS
-        end
+    end
 
-        test do
-            # A basic test to ensure the shared object file was installed.
-            assert_predicate lib/"security/pam_ssh_agent.so", :exist?
+    test do
+        # A basic test to ensure the shared object file was installed.
+        assert_predicate lib/"security/pam_ssh_agent.so", :exist?
 
-            # On macOS, we can also check if the file is signed.
-            if OS.mac?
-                (testpath/"codesign_output").write shell_output("/usr/bin/codesign -dv #{lib}/security/pam_ssh_agent.so 2>&1")
-                assert_match "Signature=adhoc", (testpath/"codesign_output").read
-                assert_match "Runtime Version", (testpath/"codesign_output").read
-            end
+        # On macOS, we can also check if the file is signed with a real identity.
+        if OS.mac?
+            (testpath/"codesign_output").write shell_output("/usr/bin/codesign -dv #{lib}/security/pam_ssh_agent.so 2>&1")
+            # This will now fail if the cert doesn't exist, which is expected before the user creates it.
+            # A successful run after cert creation will show "Authority=pam-signer".
+            assert_match "Identifier=pam_ssh_agent", (testpath/"codesign_output").read
         end
     end
+end
+
